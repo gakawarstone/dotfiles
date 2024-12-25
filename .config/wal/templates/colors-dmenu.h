1b45b29baba57b0c791b0e9c456d894b5966257a
Gakawarstone <pinnekerv@gmail.com>
2024-12-25 08:47:14 +0100

    feat: dwm with pywal appearence

---
+++ .config/wal/templates/colors-dmenu.h
@@ -0,0 +1,8 @@
+static const char norm_fg[] = "{color15}";
+static const char norm_bg[] = "{color0}";
+
+static const char sel_fg[] = "{color15}";
+static const char sel_bg[] = "{color1}";
+
+static const char out_fg[] = "{color15}";
+static const char out_bg[] = "{color14}";
---
+++ .config/wal/templates/colors-dwm.h
@@ -0,0 +1,7 @@
+static const char norm_fg[] = "{color15}";
+static const char norm_bg[] = "{color0}";
+static const char norm_border[] = "{color8}";
+
+static const char sel_fg[] = "{color15}";
+static const char sel_bg[] = "{color2}";
+static const char sel_border[] = "{color15}";
