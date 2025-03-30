1e6274d070d48d5b696e60b09597abae22ba8156
Gakawarstone <pinnekerv@gmail.com>
2025-03-30 21:08:53 +0200

    chore: remove flag from slstatus

--- packages/dwm/slstatus.config.h
+++ packages/dwm/slstatus.config.h
@@ -5,6 +5,5 @@ static const char unknown_str[] = "n/a";
 #define MAXLEN 2048
 
 static const struct arg args[] = {
-	{ cat, "%s",           "/home/gws/tmp/flag" },
 	{ keymap, "%s",           NULL },
 };
