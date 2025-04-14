72a3416b1d23a81cea6e247f3432cd5c6c3c0876
gakawarstone <pinnekerv@gmail.com>
2025-04-14 10:42:43 +0200

    chore: rename config dist to config def

---
+++ gkdots/config.def.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from _types import BinFileLinkingInfo, ConfigDirLinkingInfo
+
+
+BIN_FILES_PATH = Path("~/.local/bin/").expanduser()
+
+# TODO: all posibble configs commented
+INCLUDED_BINS = [
+    # BinFileLinkingInfo("daily", "daily"),
+    # BinFileLinkingInfo("vivaldi.hidpi", "browser"),
+    # BinFileLinkingInfo("tg.ignore_qt_scaling", "tg"),
+    # BinFileLinkingInfo("screen", "screen"),
+    # BinFileLinkingInfo("battery_notify", "battery_notify"),
+    # BinFileLinkingInfo("tgsend", "tgsend"),
+    # BinFileLinkingInfo("wal", "wal"),
+]
+
+INCLUDED_CONFIGS = [
+    # ConfigDirLinkingInfo("tmux"),
+    # ConfigDirLinkingInfo("alacritty"),
+    # ConfigDirLinkingInfo("dunst"),
+    # ConfigDirLinkingInfo("wal"),
+    # ConfigDirLinkingInfo("fish"),
+    # ConfigDirLinkingInfo("yazi"),
+]
