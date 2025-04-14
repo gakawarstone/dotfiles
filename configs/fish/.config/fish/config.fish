74423ead16b103b489e49753ff745ab30af185d5
gakawarstone <pinnekerv@gmail.com>
2025-04-14 09:57:21 +0200

    feat: add bun and lg in fish

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 set -x PATH /home/gws/.local/bin $PATH
 set -x PATH /home/gws/.cargo/bin $PATH
 set -x PATH /home/gws/.npm-global/bin $PATH
+set -x PATH /home/gws/.bun/bin $PATH
 
 set fish_greeting
 
@@ -10,6 +11,7 @@ alias c clear
 alias n nvim
 alias py python3
 alias xo xdg-open
+alias lg lazygit
 alias wgu "sudo wg-quick up wg0"
 alias wgd "sudo wg-quick down wg0"
 alias clip "xclip -selection clipboard"
@@ -25,3 +27,7 @@ end
 
 source $HOME/.env.fish
 zoxide init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
