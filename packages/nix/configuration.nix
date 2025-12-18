66e84b36a1de342c06ba2e92b2923bf5643f9a6d
Gakawarstone <pinnekerv@gmail.com>
2025-12-18 09:25:00 +0100

    chore: update nix Makefile

--- packages/nix/configuration.nix
+++ packages/nix/configuration.nix
@@ -4,67 +4,14 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./modules/boot.nix
+      ./modules/networking.nix
+      ./modules/locale.nix
+      ./modules/services.nix
+      ./modules/user.nix
+      ./modules/packages.nix
+      ./modules/desktop/xfce.nix
     ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "gwsnix";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Berlin";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
-
-  services.openssh.enable = true;
-
-  services.xserver.enable = true;
-  services.xserver.windowManager.dwm.enable = false;
-  services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  services.displayManager.defaultSession = "xfce";
-
-  users.users.gws = {
-    isNormalUser = true;
-    description = "gws";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/main.tar.gz") {
-      inherit pkgs;
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-    fastfetch
-    firefox
-    nur.repos.Ev357.helium
-    telegram-desktop
-    zed-editor
-    gnumake
-  ];
 
   system.stateVersion = "26.05";
 }
